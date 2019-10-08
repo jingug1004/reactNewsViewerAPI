37d91daac743cfc4cc4de725979e9db66b6f714f
jingug1004 <jingug1004@gmail.com>
2019-10-08 10:55:15 +0900

    191008(화) - 리액트 뉴스 뷰어 만들기

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
+import NewsList from './components/NewsList';
 import './App.css';
 
 const App = () => {
-    const [data, setData] = useState(null);
-    /*
-    const onClick = () => {
-        axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
-            setData(response.data);
-        });
-    };
-*/
-    const onClick = async () => {
-        try {
-            const response = await axios.get(
-                /*
-                'https://jsonplaceholder.typicode.com/todos/1'
-                */
-                'https://newsapi.org/v2/top-headlines?country=kr&apiKey=e9fdf950860a45daa25430d1e3475df2'
-            );
-            setData(response.data);
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
-    return (
-        <div>
-            <div>
-                <button onClick={onClick}>불러오기</button>
-            </div>
-            {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}></textarea>}
-        </div>
-    )
+    return <NewsList/>;
 }
 
 /*
